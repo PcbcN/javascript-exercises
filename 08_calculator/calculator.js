f4b735be01302167e6cac73916a2411ae6622a9b
pcb <pol@cuantofalta.es>
2022-09-27 18:21:10 +0200

    calculator hecho

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
+const add = function(a,b) {
+  return a + b;
+};
+
+const subtract = function(a,b) {
+	return a - b;
+};
+
+const sum = function(arr) {
+  let result = 0;
+    for (const item of arr){
+      result += item;
+    }
+    return result;
+};
+
+const multiply = function(arr) {
+  let result = 1;
+    for (const item of arr){
+      result *= item;
+    }
+    return result;
+
+};
+
+const power = function(a,b) {
+  let result = Math.pow(a,b);
+  return result;
 	
 };
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(a) {
+  let result = 1;
+  if (a == 0 || a == 1) {return result;
+  } else {
+    for (var i = a; i >= 1; i--){
+      result = result * i;
+    }
+    return result;
+  }
 };
 
 // Do not edit below this line
